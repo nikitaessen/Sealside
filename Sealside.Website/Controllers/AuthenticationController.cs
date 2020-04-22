9bd5b307b38dccfb3fa97085b496387da382b51a
Nick Essen <nikitaessen@gmail.com>
2020-04-23 02:06:12 +0300

    Register form added

---
+++ Sealside.Website/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sealside.Website.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        [Route("/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/register")]
+        public IActionResult Register(string login, string password)
+        {
+            return Content($"User {login} registered");
+        }
+    }
+}
--- Sealside.Website/Controllers/HomeController.cs
+++ Sealside.Website/Controllers/HomeController.cs
@@ -1,23 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sealside.Models.Database;
-using Sealside.Crosscutting;
 
 namespace Sealside.Website.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UserDbContext _context;
-
-        public HomeController(UserDbContext context)
+        public HomeController()
         {
-            _context = context;
         }
 
         public IActionResult Index()
         {
-            SampleData.Initialize(_context);
-            var users = _context.GetUsers();
-            return View(users);
+            return View();
         }
     }
 }
